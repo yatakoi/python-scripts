e9de3cb664ce7f8831daa8d5324d5c7afb62a783
Maksim <allkall@yandex.ru>
2019-11-25 14:07:43 +1000

    Обновлён sequence-collatz.py
    - Добавлена проверка введено целое число или нет. 
    - Добавлено комментирование кода.

--- sequence-collatz.py
+++ sequence-collatz.py
@@ -1,14 +1,23 @@
 # Последовательность Коллатза
 
-def collatz(number):
+# Определяем переменную collatz.
+def collatz (number):
     if number % 2 == 0:
         return number // 2
     elif number % 2 == 1:
         return 3 * number + 1
+    else:
+        return 0
 
-number = int(input())
-
-while number != 1:
-    print(number)
-    number = collatz(number)
-print(1)
+# Добавляем исключение try - expect.
+# Исключение проверяет введено ли целое число. 
+# Если не введено целое число, то появится ошибка "ValueError" и оператор except сообщит об этом.
+try:
+    number = int (input())
+except ValueError:
+    print ('Вы должны ввести целое число. Повторите запуск скрипта и введите ЦЕЛОЕ число.')
+else: # Иначе если введено целое число, то цикл while будет выполняться до тех пор, пока значение не станет 1.
+    while number != 1:
+        print (number)
+        number = collatz (number)
+    print (1)
